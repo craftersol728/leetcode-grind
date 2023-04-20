49a548665c2c20339936daa6d1abd1fa7f6bdac2
Jerry Wan <craftersol728@gmail.com>
2023-04-19 21:10:07 -0700

    added daily temperture

---
+++ dailyTemperature.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    
+    const stackTemp = [];
+    let result = Array(temperatures.length).fill(0);
+
+    for (let i = 0; i<temperatures.length;i++){
+        while()
+    }
+};
