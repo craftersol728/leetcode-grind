08aa981869aabaf51e67caa0aa4b150aecb87511
Jerry Wan <craftersol728@gmail.com>
2023-04-24 13:36:47 -0700

    acttually solved water container question

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,28 +1,27 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function(height) {
+let left = 0;
+let right = height.length - 1;
+let maxArea = 0;
 
-    let left = 0; 
-    let right = numbers.length - 1;
-    let maxArea = 0;
+while (left < right) {
+    // Calculate the minimum height
+    let minHeight = Math.min(height[left], height[right]);
 
-    while (left < right){
-        let minHeight = Math.min(height[left],height[right]);
+    // Calculate the distance between the pointers
+    let distance = right - left;
 
-        let distance = right - left;
+    // Calculate the area by multiplying minHeight by distance
+    let area = minHeight * distance;
 
-        let area = minHeight * distance;
+    // Update maxArea if the current area is greater
+    maxArea = Math.max(maxArea, area);
 
-        maxArea = Math.max(maxArea, area);
-
-        if(height[left] < height[right]){
-            left ++;
-        } else {
-            right ++;
-        }
+    // Move the pointer pointing to the shorter line towards the center
+    if (height[left] < height[right]) {
+        left++;
+    } else {
+        right--;
     }
+}
 
-    return maxArea;
-};
+// Return the maximum area
+return maxArea;
