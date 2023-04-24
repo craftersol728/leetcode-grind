a318f1aa35cf56dfd1f2f538331656cfda966cb3
Jerry Wan <craftersol728@gmail.com>
2023-04-24 14:29:02 -0700

    solved trapping rain water

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,27 +1,33 @@
-let left = 0;
-let right = height.length - 1;
-let maxArea = 0;
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
 
-while (left < right) {
-    // Calculate the minimum height
-    let minHeight = Math.min(height[left], height[right]);
+    while (left < right) {
+        // Calculate the minimum height
+        let minHeight = Math.min(height[left], height[right]);
 
-    // Calculate the distance between the pointers
-    let distance = right - left;
+        // Calculate the distance between the pointers
+        let distance = right - left;
 
-    // Calculate the area by multiplying minHeight by distance
-    let area = minHeight * distance;
+        // Calculate the area by multiplying minHeight by distance
+        let area = minHeight * distance;
 
-    // Update maxArea if the current area is greater
-    maxArea = Math.max(maxArea, area);
+        // Update maxArea if the current area is greater
+        maxArea = Math.max(maxArea, area);
 
-    // Move the pointer pointing to the shorter line towards the center
-    if (height[left] < height[right]) {
-        left++;
-    } else {
-        right--;
+        // Move the pointer pointing to the shorter line towards the center
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
     }
-}
 
-// Return the maximum area
-return maxArea;
+    // Return the maximum area
+    return maxArea;
+};
