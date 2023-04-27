6ed98814c41f7948871f19596746233727ffedd8
Jerry Wan <craftersol728@gmail.com>
2023-04-26 20:33:53 -0700

    solved binary search and started 2d matrix

---
+++ 2dMatrix.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const numRows = matrix.length;
+    const numCols = matrix[0].length;
+    
+    let startRow = 0;
+    let endRow = numRows - 1;
+    let targetRow = -1;
+
+        while (startRow <= endRow){
+            const middleRow = Math.floor((startRow + endRow)/2);
+
+            if (matrix[middleRow][0] <= target && matrix[middleRow][numCols - 1] >= target){
+                targetRow = middleRow;
+                break;
+            }
+
+            if (matrix[middleRow][0] > target){
+                endRow = middleRow -1;
+            } else {
+                startRow = middleRow+1
+            }
+        }
+
+        if (targetRow === -1){
+            return false;
+        }
+        
+        return binarySearch(matrix[targetRow], target, 0, numCols - 1);
+};
