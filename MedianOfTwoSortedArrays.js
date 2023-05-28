55cfda81ed222ff11fd593d101c0b09714878698
Jerry Wan <craftersol728@gmail.com>
2023-05-27 19:54:24 -0700

    solved stocks

--- MedianOfTwoSortedArrays.js
+++ MedianOfTwoSortedArrays.js
@@ -4,5 +4,9 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    
+    const array1 = nums1;
+    const array2 = nums2;
+
+    let totalLength = nums1.length + nums2.length
+    let middle = totalLength / 2  
 };
